d76ee32cde0a27aba45924db4127a550c8222316
Eric Foard <foard265@gmail.com>
2016-03-19 20:48:00 -0700

    recursive version, with bugs

--- main.js
+++ main.js
@@ -1,48 +1,54 @@
 var $ = require('jQuery');
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: true });
 
-crawl(nightmare, 'http://vancouver.craigslist.ca/search/apa?postedToday=1');
+var nm = crawl(false, 'http://vancouver.craigslist.ca/search/apa?postedToday=1', {}, 0, 0, 0);
 
-function crawl(nightmare, dest) {
-    var curr_range, max_range, listings = [];
+function crawl(nightmare, dest, listings, curr_range, max_range, n) {
 
-    nightmare.on('javascript', function(err) {
-        console.log(err);
-    })
-    .on('error', function(err) {
-        console.log(err);
-    })
-    .goto(dest)
-    .wait(1000);
+    // termination condition
+    // if (curr_range >= max_range && curr_range > 0 && max_range > 0 &&
+    //     nightmare) {
+    if (n >= 3) {
+            console.log('TERMINATING');
+            nightmare.end()
+            .then(listings, function (listings) {
+                console.log(listings);
+                console.log('listings LENGTH ',listings.length);
+            });
+            return;
+            //return listings;
+        }
 
-        do {
-            listings.push(nightmare.evaluate(function () {
-                var nodes = document.getElementsByClassName('hdrlnk');
-                var link, title, temp_listings;
-
-                for (var i=0; i<nodes.length; i++) {
-                    link = nodes[i].getAttribute('href');
-                    title = nodes[i].children[0].innerHTML;
-                    temp_listings[link] = title;
-                }
-
-                curr_range = document.querySelector('.rangeTo');
-                max_range = document.querySelector('.totalcount');
-
-                console.log(temp_listings)
-
-                return temp_listings;
-
-            }));
-
-            nightmare.click('.next')
+        // first call
+        if (!nightmare) {
+            console.log('FIRST CALL');
+            nightmare = Nightmare({ show: true });
+            nightmare.on('javascript', function(err) {
+                console.log(err);
+            })
+            .on('error', function(err) {
+                console.log(err);
+            })
+            .goto(dest)
             .wait();
-        } while (curr_range < max_range);
+        }
 
-    nightmare.end()
-    .then(function (listings) {
-        console.log(listings);
-        console.log('listings LENGTH ',listings.length);
-    });
-}
+        nightmare.evaluate(listings, function (listings) {
+            var nodes = document.getElementsByClassName('hdrlnk');
+            var link, title;
+
+            for (var i=0; i<nodes.length; i++) {
+                link = nodes[i].getAttribute('href');
+                title = nodes[i].children[0].innerHTML;
+                listings[link] = title;
+            }
+
+            curr_range = document.querySelector('.rangeTo');
+            max_range = document.querySelector('.totalcount');
+        });
+
+        nightmare.click('.next')
+        .wait();
+
+        return crawl(nightmare, dest, listings, curr_range, max_range, ++n);
+    }
