e993a079e6ff874b602b41f213734e53d8024b1f
Eric Foard <foard265@gmail.com>
2016-03-19 20:53:49 -0700

    updated control flow

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var $ = require('jQuery');
 var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: true });
 
-crawl(nightmare, 'http://vancouver.craigslist.ca/search/apa?postedToday=1');
+crawl(nightmare, 'http://vancouver.craigslist.ca/search/apa?postedToday=1&max_price=3500&bedrooms=2&bathrooms=2&pets_dog=1');
 
 function crawl(nightmare, dest) {
-    var curr_range, max_range, listings = [];
+    var curr_range, max_range, listings = {};
 
     nightmare.on('javascript', function(err) {
         console.log(err);
@@ -14,34 +14,28 @@ function crawl(nightmare, dest) {
         console.log(err);
     })
     .goto(dest)
-    .wait(1000);
+    .wait();
 
-        do {
-            listings.push(nightmare.evaluate(function () {
-                var nodes = document.getElementsByClassName('hdrlnk');
-                var link, title, temp_listings;
+    do {
+        nightmare.evaluate(listings, function (listings) {
+            var nodes = document.getElementsByClassName('hdrlnk');
+            var link, title;
 
-                for (var i=0; i<nodes.length; i++) {
-                    link = nodes[i].getAttribute('href');
-                    title = nodes[i].children[0].innerHTML;
-                    temp_listings[link] = title;
-                }
+            for (var i=0; i<nodes.length; i++) {
+                link = nodes[i].getAttribute('href');
+                title = nodes[i].children[0].innerHTML;
+                listings[link] = title;
+            }
 
-                curr_range = document.querySelector('.rangeTo');
-                max_range = document.querySelector('.totalcount');
-
-                console.log(temp_listings)
-
-                return temp_listings;
-
-            }));
-
-            nightmare.click('.next')
-            .wait();
-        } while (curr_range < max_range);
+            curr_range = document.querySelector('.rangeTo');
+            max_range = document.querySelector('.totalcount');
+        });
+        nightmare.click('.next')
+        .wait();
+    } while (curr_range < max_range);
 
     nightmare.end()
-    .then(function (listings) {
+    .then(listings, function (listings) {
         console.log(listings);
         console.log('listings LENGTH ',listings.length);
     });
